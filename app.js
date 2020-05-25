9a5c1bbd0a18f9c6df18fa93047f6e25ecfb2d0d
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-05-25 22:53:42 +1200

    restful APIs running from MySQL database

--- app.js
+++ app.js
@@ -1,29 +1,106 @@
+// Required variables 
 const express = require ('express');
 const app = express();
 const morgan = require('morgan');
+const mysql = require('mysql');
 
-app.use(morgan('short'));
+// Required using
+app.use(morgan('combined'));
 
-// localhost:3001
+// Database connection config
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'P@ssword1',
+    database: 'got_apples_db'
+});
+
+// Start server on localhost:3333/
 app.listen(3333, () => {
-    console.log('RESTful API server is running and listening on port 3333');
+    console.log('Got Apples! RESTful API service is running on port 3333');
 });
 
 
+
+// working specific user api
+// example -  http://localhost:3333/user/Fitz
+
+app.get('/user/:username', (req, res) => {
+    const username = req.params.username
+    const queryString = "SELECT * FROM user WHERE user_name = ?"
+    connection.query(queryString, [username], (err, rows, fields) => {
+        if (err) {
+            console.log("Get user failed:" + err)
+            res.sendStatus(500)
+            return
+        };
+        console.log(rows)
+        res.json(rows)
+    });
+});
+
+
+// working all users api
+app.get('/users', (req, res) => {
+
+    const queryString = "SELECT * FROM user"
+    connection.query(queryString, (err, rows, fields) => {
+        console.log(rows);
+        res.json(rows);
+    });
+
+    //res.end();
+});
+
+
+
+// working specific user api
+app.get('/orchard/:orchard_name', (req, res) => {
+
+    const orchard_name = req.params.orchard_name
+    const queryString = "SELECT * FROM orchard WHERE orchard_name = ?"
+    connection.query(queryString, [orchard_name], (err, rows, fields) => {
+        console.log(rows);
+        res.json(rows);
+    });
+    //res.end();
+});
+
+
+// working all orchards api
+app.get('/orchards', (req, res) => {
+
+    const queryString = "SELECT * FROM orchard"
+    connection.query(queryString, (err, rows, fields) => {
+        console.log(rows);
+        res.json(rows);
+    });
+
+    //res.end();
+});
 
 app.get("/", (req, res) => {
-    console.log("Got a root route request");
-    res.send("Hello!!");
+    res.send("Got Apples! RESTful API service");
 });
 
-app.get("/users", (req, res) => {
+// app.get("/users", (req, res) => {
 
-    let user = {
-        first_name: "Callum",
-        second_name: "McFadgen"
-    };
-    res.json(user);
-});
+//     const user1 = {
+//         first_name: "Callum",
+//         second_name: "McFadgen"
+//     };
+//     const user2 = {
+//         first_name: "Jason",
+//         second_name: "McDonald"
+//     };
+//     const user3 = {
+//         first_name: "Corrina",
+//         second_name: "Busby"
+//     };
+
+//     res.json([user1, user2, user3]);
+// });
 
 
 
