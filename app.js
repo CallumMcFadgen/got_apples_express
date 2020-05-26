af80e10651b46720f0666533d89c117a7d3cdbe2
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-05-26 12:54:04 +1200

    some apis running for get users and user and POST but PATCH and DELETE are a bit weird

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
-// Required variables 
+// Required variables (imports)
 const express = require ('express');
 const app = express();
 const morgan = require('morgan');
 const mysql = require('mysql');
+const bodyParser = require ('body-parser');
 
-// Required using
-app.use(morgan('combined'));
+// Required usings (middleware)
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('short'));
+app.use(express.static('./public'));
 
-// Database connection config
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'P@ssword1',
-    database: 'got_apples_db'
-});
+// Connection configuration
+function MySQLConnection () {
+    return mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'P@ssword1',
+        database: 'got_apples_db'
+    });
+};
 
 // Start server on localhost:3333/
 app.listen(3333, () => {
     console.log('Got Apples! RESTful API service is running on port 3333');
 });
 
+// homepage message
+app.get("/", (req, res) => {
+    res.send("Got Apples! RESTful API service");
+});
 
 
-// working specific user api
-// example -  http://localhost:3333/user/Fitz
 
-app.get('/user/:username', (req, res) => {
+// USER API's //////////////////////////////////////////////////////////////////
+
+// Get a user by username // example - http://localhost:3333/get_user/Fitz
+app.get('/get_user/:username', (req, res) => {
     const username = req.params.username
     const queryString = "SELECT * FROM user WHERE user_name = ?"
-    connection.query(queryString, [username], (err, rows, fields) => {
+    MySQLConnection().query(queryString, [username], (err, rows, fields) => {
         if (err) {
-            console.log("Get user failed:" + err)
+            console.log("Get user " + username + " failed: " + err)
             res.sendStatus(500)
             return
         };
@@ -41,17 +51,90 @@ app.get('/user/:username', (req, res) => {
 });
 
 
-// working all users api
-app.get('/users', (req, res) => {
-
+// Get all users // example - http://localhost:3333/get_users
+app.get('/get_users', (req, res) => {
     const queryString = "SELECT * FROM user"
-    connection.query(queryString, (err, rows, fields) => {
+    MySQLConnection().query(queryString, (err, rows, fields) => {
+        if (err) {
+            console.log("Get users failed: " + err)
+            res.sendStatus(500)
+            return
+        };
         console.log(rows);
         res.json(rows);
     });
-
-    //res.end();
 });
+
+
+// Post a new user
+app.post('/post_user',(req, res) => { 
+    const firstName = req.body.create_first_name
+    const lastName = req.body.create_last_name
+    const username = req.body.create_username
+    const password = req.body.create_password
+    const emailAddress = req.body.create_email_address
+    const phoneNumber =  req.body.create_phone_number
+    const addressLine1 = req.body.create_address_line_1
+    const addressLine2 = req.body.create_address_line_2
+    const region = req.body.create_region
+    const city = req.body.create_city
+    const zipCode = req.body.create_zip_code
+    const memberStatus = req.body.create_member_status
+
+    const queryString = "INSERT INTO user (user_name, first_name, last_name, password, email_address, phone_number, address_line_1, address_line_2, region, city, zip_code, got_apples_member) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    MySQLConnection().query(queryString, [username, firstName, lastName, password, emailAddress, phoneNumber, addressLine1, addressLine2, region, city, zipCode, memberStatus], (err, results, fields) => {
+        if (err) {
+            console.log("Post user " + username + " failed: " + err)
+            res.sendStatus(500)
+            return
+        };
+        console.log("Post user succeded: user " + username + " added")
+        res.end()
+    });
+});
+
+
+// // Patch an existing user
+// app.patch('/patch_user/:username',(req, res) => { 
+
+//     const firstName = req.body.create_first_name
+
+//     const queryString = "UPDATE user SET (first_name) WHERE user_name = ?"
+//     MySQLConnection().query(queryString, [username, firstName], (err, results, fields) => {
+//         if (err) {
+//             console.log("Patch user " + username + " failed: " + err)
+//             res.sendStatus(500)
+//             return
+//         };
+//         console.log("Patch user succeded: user " + username + " added")
+//         res.end()
+//     });
+// });
+
+// Delete a user by username // example - http://localhost:3333/delete_user/
+// wont work in a browser?
+app.delete('/delete_user/:username', (req, res) => {
+    const username = req.params.username
+    //const username = req.body.delete_username
+    const queryString = "DELETE FROM user WHERE user_name = ?"
+    MySQLConnection().query(queryString, [username], (err, results, fields) => {
+        if (err) {
+            console.log("Delete user " + username + " failed: " + err)
+            res.sendStatus(500)
+            return
+        };
+        console.log("Delete user succeded: user " + username + " deleted")
+        res.end()
+    });
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 
 
 
@@ -60,7 +143,7 @@ app.get('/orchard/:orchard_name', (req, res) => {
 
     const orchard_name = req.params.orchard_name
     const queryString = "SELECT * FROM orchard WHERE orchard_name = ?"
-    connection.query(queryString, [orchard_name], (err, rows, fields) => {
+    MySQLConnection().query(queryString, [orchard_name], (err, rows, fields) => {
         console.log(rows);
         res.json(rows);
     });
@@ -72,7 +155,7 @@ app.get('/orchard/:orchard_name', (req, res) => {
 app.get('/orchards', (req, res) => {
 
     const queryString = "SELECT * FROM orchard"
-    connection.query(queryString, (err, rows, fields) => {
+    MySQLConnection().query(queryString, (err, rows, fields) => {
         console.log(rows);
         res.json(rows);
     });
@@ -80,27 +163,9 @@ app.get('/orchards', (req, res) => {
     //res.end();
 });
 
-app.get("/", (req, res) => {
-    res.send("Got Apples! RESTful API service");
-});
 
-// app.get("/users", (req, res) => {
 
-//     const user1 = {
-//         first_name: "Callum",
-//         second_name: "McFadgen"
-//     };
-//     const user2 = {
-//         first_name: "Jason",
-//         second_name: "McDonald"
-//     };
-//     const user3 = {
-//         first_name: "Corrina",
-//         second_name: "Busby"
-//     };
 
-//     res.json([user1, user2, user3]);
-// });
 
 
 
